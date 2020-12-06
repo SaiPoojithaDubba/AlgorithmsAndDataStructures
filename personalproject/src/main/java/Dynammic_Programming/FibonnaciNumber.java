e00aec20708c7aef996d90c8baaf71401c7aa29f
h282772 <SaiPoojitha.Dubba@honeywell.com>
2020-12-06 13:26:17 +0530

    dynammic programming

---
+++ personalproject/src/main/java/Dynammic_Programming/FibonnaciNumber.java
@@ -0,0 +1,22 @@
+package Dynammic_Programming;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FibonnaciNumber {
+    public static void main(String[] args) {
+        FibonnaciNumber f = new FibonnaciNumber();
+        System.out.println(f.getNthFibonnaciNumber(25, new HashMap<Integer, Integer>()));
+    }
+
+    public Integer getNthFibonnaciNumber(int number, Map<Integer, Integer> memo) {
+        if (memo.containsKey(number)) return memo.get(number);
+        if (number <= 2) return 1;
+        int targetSum = getNthFibonnaciNumber(number - 1, memo) + getNthFibonnaciNumber(number - 2, memo);
+        memo.put(number, targetSum);
+        return memo.get(number);
+    }
+    //Time & Space Complexities with and without memoization
+    //BruteForce: Time - O(2^n) Space - O(n)
+    //After Memoization: Time - O(n) Space - O(n)
+}
